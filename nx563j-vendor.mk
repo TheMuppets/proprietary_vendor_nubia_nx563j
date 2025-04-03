ece6a14a1b5eb1bdf022171c7cb873f06b3a7031
Vladimir Mikhailov <beykerykt@gmail.com>
2025-04-03 22:09:04 +0900

    nx563j: Patch camera blobs to appease elfchecks
    Change-Id: Iac17020835636137096a3f83138feef2bd527057

--- nx563j-vendor.mk
+++ nx563j-vendor.mk
@@ -358,6 +358,7 @@ PRODUCT_PACKAGES += \
     gatekeeper.msm8998 \
     keystore.msm8998 \
     nfc_nci.bcm2079x.msm8998 \
+    libAltek_Alignment \
     libArcsoftAlgorithm \
     libDualCamera_Calibration \
     libqfp-service \
